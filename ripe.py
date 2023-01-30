75487d90d17f02114657210684db566915aed3b4
John Mark Gabriel Caguicla <jmg.caguicla@guarandoo.me>
2023-01-30 22:29:54 +0800

    add extra handling for route/route6 objects * strips the AS number at the end of the key for route/route6 objects so that `ripe_update` and `ripe_create` plays nice

--- ripe.py
+++ ripe.py
@@ -8,7 +8,7 @@ from pprint import pprint
 import requests
 import sys
 import yaml
-
+import re
 
 def ripe_create(db, pwd, json_output, key, type, dryrun, object_entries):
     """
@@ -298,7 +298,10 @@ if __name__ == "__main__":
         yml_objects = yaml_parser(args.objects)
         for key in yml_objects.keys():
             type = list(yml_objects[key][0].keys())[0]
-            name = key
+            if type == "route" or type == "route6":
+                name = re.sub('AS\d+$', '', key)
+            else:
+                name = key
             print("")
             print(type, key)
             ripe_object = ripe_get(args.db, type, name, yml_objects[key])
@@ -326,7 +329,7 @@ if __name__ == "__main__":
                         args.db,
                         pwd,
                         json_output,
-                        key,
+                        name,
                         type,
                         args.dryrun,
                         yml_objects[key],
@@ -336,7 +339,7 @@ if __name__ == "__main__":
                 print(f"Object does not exists in the RIPE database")
                 json_output = yaml_to_json(yml_objects[key])
                 ripe_create(
-                    args.db, pwd, json_output, key, type, args.dryrun, yml_objects[key]
+                    args.db, pwd, json_output, name, type, args.dryrun, yml_objects[key]
                 )
 
     # else if cmdline argument for search is given
